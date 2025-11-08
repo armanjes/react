20f9af4e63e60759418ebeec09be27c2aa9f9f0e
Arman <mdarmanjes@gmail.com>
2025-11-08 10:50:04 +0600

    useReducer()

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
-import Card from "./components/Card"
-import Welcome from "./components/Welcome"
+/**
+ * syntax: const [state, dispatch] = useReducer(reducer, initialState)
+ * reducer: reducer is a pure function with two arguments state & action. This function specifies how the state gets updated.
+ * initialState: The value from which initial state gets calculated.
+ * This hook returns an array of two values state & dispatch.
+ */
+
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment_age":
+      return { ...state, age: state.age + 1 };
+    case "change_name":
+      return { ...state, name: action.n };
+  }
+    throw Error("Unknown action: " + action.type);
+}
+
+function stateObj() {
+  return { name: "taylor", age: 42 };
+}
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, stateObj());
+
   return (
     <>
-      <Welcome name="John Doe" />
-      <h1>this is useReducer Branch</h1>
-      <Card>
-        <h2>Welcome</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
-      </Card>
-      <Card color="yellow">
-        <h2>Warning</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
-      </Card>
+      <input
+        type="text"
+        value={state.name}
+        onChange={(e) =>
+          dispatch({ type: "change_name", n: e.target.value })
+        }
+        placeholder="Type here"
+      />
+      <button onClick={() => dispatch({type: "increment_age"})}>Increment Age</button>
+      <p>
+        Hello, {state.name}. You are {state.age}.
+      </p>
     </>
   );
-}
-export default App
+};
+export default App;
